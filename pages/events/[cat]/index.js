f8886de3022921ca8f2a4132a142b6148872760b
Matthew Rada <matthewr7.christopher@gmail.com>
2023-05-01 19:04:17 -0400

    first commit

---
+++ pages/events/[cat]/index.js
@@ -0,0 +1,15 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Our Single Event</h1>
+      <a href='/event/event1'> Event1</a>
+      <a href='/event/event2'> Event2</a>
+      <a href='/event/event3'> Event3</a>
+      <a href='/event/event4'> Event4</a>
+      <a href='/event/event5'> Event5</a>
+      <a href='/event/event6'> Event6</a>
+    </div>
+  )
+}
+
+export default Page
